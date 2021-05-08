d97a24bbe0fe8ff40d391fa8a59cb845dec1d540
darshjain <darshj786@gmail.com>
2021-05-08 16:17:20 +0530

    Update #9 - Built Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PollCreateComponent } from './poll-create/poll-create.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [
-    AppComponent,
-    PollCreateComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, PollCreateComponent],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
