36736d6ce3cab88e7e6ccd32fdf598d20db191ed
darshjain <darshj786@gmail.com>
2021-05-08 17:35:28 +0530

    Update #10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PollCreateComponent } from './poll-create/poll-create.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PollComponent } from './poll/poll.component';
+
 @NgModule({
-  declarations: [AppComponent, PollCreateComponent],
+  declarations: [AppComponent, PollCreateComponent, PollComponent],
   imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
