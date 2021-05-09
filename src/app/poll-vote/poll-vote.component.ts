f0cb579211de8dd9da50dd1f39066773e2ec63c7
darshjain <darshj786@gmail.com>
2021-05-09 16:17:34 +0530

    Update #13

--- src/app/poll-vote/poll-vote.component.ts
+++ src/app/poll-vote/poll-vote.component.ts
@@ -1,24 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import ApexCharts from 'apexcharts';
 
 @Component({
   selector: 'app-poll-vote',
   templateUrl: './poll-vote.component.html',
-  styleUrls: ['./poll-vote.component.scss']
+  styleUrls: ['./poll-vote.component.scss'],
 })
 export class PollVoteComponent implements OnInit {
+  @Input() voted: boolean;
+  @Input() options:string[];
+  @Input() results:number[];
+  @Input() question:string;
 
-  options=["Monday","Tuesday","Wednesday"]
   voteform: FormGroup;
-  constructor(private fb:FormBuilder) {
-    this.voteform=this.fb.group({
-      selected:this.fb.control("",[Validators.required]),
+  constructor(private fb: FormBuilder) {
+    this.voteform = this.fb.group({
+      selected: this.fb.control('', [Validators.required]),
     });
   }
 
   ngOnInit(): void {
+    if (this.voted) {
+      this.generateChart();
+    }
   }
-  submitForm(){
+  submitForm() {
     console.log(this.voteform.value);
+  }
+  generateChart() {
+    const options: ApexCharts.ApexOptions = {
+      series: [
+        {
+          data: this.results,
+        },
+      ],
+      chart: {
+        height: 350,
+        type: 'bar',
+      },
+      plotOptions: {
+        bar: {
+          columnWidth: '45%',
+          distributed: true,
+        },
+      },
+      legend: {
+        show: false,
+      },
+      xaxis: {
+        categories: this.options,
+      },
+    };
+    const chart = new ApexCharts(
+      document.getElementById('poll-results'),
+      options
+    );
+    chart.render();
   }
 }
